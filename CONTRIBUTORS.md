09ffc1a49bc08255665851e36b862c0cd2b31f96
malakar-soham <malakar_soham@outlook.com>
2020-12-25 22:22:19 +0530

    Cleaned code for deployment

--- CONTRIBUTORS.md
+++
@@ -1,5 +0,0 @@
-# Contributors
-
-This is the Esports Zealot hall of fame! All contributors of source code, or documentation, or tests are eligible to be added to this list.
-- Soham Malakar ([@malakar_soham](https://github.com/malakar-soham))
-- Swastik Malakar ([@malakar-swastik](https://github.com/malakar-swastik))
--- app.py
+++ app.py
@@ -1,24 +1,52 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, request
+import sqlite3
+import json
+from werkzeug.utils import redirect
 
 app = Flask(__name__, template_folder='Frontend', static_folder='Frontend/assets')
 
-
 @app.route('/')
-def index():
+def index(): 
     return render_template('index.html')
-
-
-@app.route('/slangs.html')
+# server start kro mere pr blank dikha rha
+@app.route('/slangs')
 def slang():
     return render_template('slangs.html')
 
+def make_dicts(cursor, row):
+    return dict((cursor.description[idx][0], value) for idx, value in enumerate(row))
 
-@app.route('/about.html')
+def test_make_json(cur, row):
+    r = [dict((cur.description[i][0], value) \
+               for i, value in enumerate(row)) for row in cur.fetchall()]
+    return r
+
+@app.route('/api/search')
+def search():
+    query = request.args.get('query', None)
+    if query:
+        conn = sqlite3.connect('database.db')
+        cursor = conn.cursor()
+        rows = cursor.execute("SELECT * FROM Zealot WHERE slang=?", (query,))
+        conn.commit()
+
+        return json.dumps(make_dicts(cursor, rows))
+    
+    return json.dumps({"message": "No query found"}), 400
+
+@app.route('/api/getAllSlangs')
+def get_all_slangs():
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    rows = cursor.execute("SELECT * FROM Zealot")
+    conn.commit()
+    return json.dumps(test_make_json(cursor, rows))
+
+
+@app.route('/about')
 def about():
     return render_template('about.html')
 
-
-app.add_url_rule('/index.html', '/', index)
 
 if __name__ == '__main__':
     app.run(debug=True)
